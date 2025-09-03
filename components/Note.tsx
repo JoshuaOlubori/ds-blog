90a80694e486a7c335f8744248c3fe20f4f057ba
JoshuaOlubori <joshuaolubori@gmail.com>
2025-09-03 11:36:27 +0100

    Refactor code structure for improved readability and maintainability

--- components/Note.tsx
+++ components/Note.tsx
@@ -1,7 +1,7 @@
 export default function Note({ children }) {
   return (
-    <div className="rounded-md border-l-4 border-blue-500 bg-blue-50 p-4 text-sm dark:text-gray-700">
+    <div className="rounded-md border-l-4 border-blue-500 bg-blue-50 p-4 text-sm dark:text-gray-700 dark:invert">
       <strong className="text-blue-700">Note:</strong> {children}
     </div>
-  )
+  );
 }
