387772ebff2a62a2ab4dfaa6058068c2a764de02
JoshuaOlubori <joshuaolubori@gmail.com>
2025-09-03 11:56:51 +0100

    prettier errors fixed

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,11 +1,11 @@
-import TOCInline from 'pliny/ui/TOCInline'
-import Pre from 'pliny/ui/Pre'
-import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
-import type { MDXComponents } from 'mdx/types'
-import Image from './Image'
-import CustomLink from './Link'
-import TableWrapper from './TableWrapper'
-import Note from './Note'
+import TOCInline from "pliny/ui/TOCInline";
+import Pre from "pliny/ui/Pre";
+import BlogNewsletterForm from "pliny/ui/BlogNewsletterForm";
+import type { MDXComponents } from "mdx/types";
+import Image from "./Image";
+import CustomLink from "./Link";
+import TableWrapper from "./TableWrapper";
+import Note from "./Note";
 
 export const components: MDXComponents = {
   Image,
@@ -15,4 +15,4 @@ export const components: MDXComponents = {
   table: TableWrapper,
   BlogNewsletterForm,
   Note,
-}
+};
